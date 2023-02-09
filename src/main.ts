51e5305925ada8ed8995aa2fa98b2fe19c43d2e4
Yuren Ju <yurenju@gmail.com>
2023-02-09 20:30:31 +0800

    add session support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'test-session-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
