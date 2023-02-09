c813d93649e6cfedb52d827d0e099373f7e19795
Yuren Ju <yurenju@gmail.com>
2023-02-09 20:20:12 +0800

    added client code

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import { ethers } from 'ethers';
+import { SiweMessage } from 'siwe';
+
+async function login() {
+  console.log('login');
+}
+
+document.getElementById('siwe-login').addEventListener('click', login);
